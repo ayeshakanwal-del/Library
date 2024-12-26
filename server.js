1d353541e155784ab6458e43723f6c35df9975d6
ayeshakanwal-del <144205766+ayeshakanwal-del@users.noreply.github.com>
2024-12-26 20:18:06 +0500

    models, routes, controllers for library management system

--- server.js
+++ server.js
@@ -2,27 +2,25 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const routes = require('./routes');
-//const errorHandler = require('./middleware/errorHandler');
 
-// Load environment variables from .env file
+
+
 dotenv.config();
 
-// Connect to the database
+
 connectDB();
 
 const app = express();
 
-// Middleware to parse JSON bodies
+
 app.use(express.json());
 
-// Routes
+
 app.use('/api', routes);
 
-// Error handler middleware (must be placed after routes)
-//app.use(errorHandler);
 
-// Set the port from environment variable or default to 5000
+
 const PORT = process.env.PORT || 5000;
 
-// Start the server
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
